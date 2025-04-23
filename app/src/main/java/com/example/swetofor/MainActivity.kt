81909000a57873b8220aa5f8b674c6d083e99846
L2MBO <157141954+L2MBO@users.noreply.github.com>
2025-04-23 20:11:51 +0300

    Переменные переименованы

--- app/src/main/java/com/example/swetofor/MainActivity.kt
+++ app/src/main/java/com/example/swetofor/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.swetofor
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import com.example.swetofor.databinding.ActivityMainBinding
 import androidx.core.content.ContextCompat
@@ -9,74 +8,67 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var currentLight = 1 // 0 - красный, 1 - желтый, 2 - зеленый
+    private var currentLight = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Используем ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Установка начальных цветов
         setInitialColors()
 
-        // Обработчик нажатия на кнопку "Start"
-        binding.button4.setOnClickListener {
+        binding.startButton.setOnClickListener {
             changeLight()
         }
     }
 
     private fun setInitialColors() {
-        binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
-        binding.button2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Желтый
-        binding.button3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
+        binding.red.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
+        binding.yellow.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Желтый
+        binding.green.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
         currentLight = 3
     }
 
     private fun changeLight() {
         when (currentLight) {
             0 -> {
-                binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
-                binding.button2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark)) // Серый
-                binding.button3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Зеленый
+                binding.red.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                binding.yellow.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+                binding.green.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
                 currentLight = 1
             }
-            1 -> { // Зеленый сигнал
-                binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
-                binding.button2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Желтый
-                binding.button3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_green_dark)) // Серый
+            1 -> {
+                binding.red.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                binding.yellow.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                binding.green.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_green_dark))
                 currentLight = 2
             }
             2 -> {
-            binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
-            binding.button2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark)) // Серый
-            binding.button3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Зеленый
-            currentLight = 3
+                binding.red.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                binding.yellow.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+                binding.green.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                currentLight = 3
             }
             3 -> {
-                binding.button.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_red_dark)) // Красный
-                binding.button2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
-                binding.button3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray)) // Серый
+                binding.red.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
+                binding.yellow.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+                binding.green.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
                 currentLight = 0
             }
         }
     }
 
-    /**
-     * Сохраняет состояние текущего светофора при повороте устройства
-     */
+    //Сохраняет состояние при повороте
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt("currentLight", currentLight)
     }
 
-    /**
-     * Восстанавливает состояние текущего светофора после поворота устройства
-     */
+    //Восстанавливает состояние после поворота
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         currentLight = savedInstanceState.getInt("currentLight")
-        setInitialColors() // Перезапускаем начальные цвета
+        setInitialColors()
     }
 }
